8a246a06a37a452c72311c7574a2bb2c2d84cb18
emmanuel <ogahemmanuel75@gmail.com>
2022-07-25 13:44:08 +0000

    Project setup updated test

--- src/index.js
+++ src/index.js
@@ -1,59 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import {applyPolyfills, defineCustomElements} from 'h8k-components/loader'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
-});
-
-client
-  .query({
-    query: gql`
-    query {
-      category {
-        name
-        products {
-          id
-          name
-          inStock
-          gallery
-          description
-          category
-          brand
-          prices {
-            currency {
-              label
-              symbol
-            }
-            amount
-          }
-        }
-      }
-    }
-    `
-  })
-  .then(result => console.log(result));
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
+applyPolyfills().then(() => {
+    defineCustomElements(window)
+})
