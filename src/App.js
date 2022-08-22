76426dccc290865a68d5d38bc49651dd9a911f05
emmanuel <ogahemmanuel75@gmail.com>
2022-08-22 22:54:59 +0100

    Convert duration input field in hours or minutes

--- src/App.js
+++ src/App.js
@@ -7,42 +7,26 @@ import { Movieform, Movieslist, Search } from './components'
 const title = 'Favorite Movie Directory'
 
 function App() {
-  const [formInput, setFormInput] = useState({
-    name: "",
-    ratings: "",
-    duration: ""
-   });
+  const [movieData, setMovieData] = useState([]);
   
-    const [movieData, setMovieData] = useState([]);
-  
-  const changeHandler = (e) => {
-    e.preventDefault();
-    setFormInput({
-      ...formInput,
-      [e.target.id]: e.target.value,
-    });
+  const addMovie = (name, rating, duration) => {
+    setMovieData((movieData) => [
+      ...movieData,
+      {
+        name,
+        rating,
+        duration,
+      },
+    ]);
   };
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //console.log(formInput)
-    if(formInput){
-      setMovieData((list) => [...list, formInput]);
-      setFormInput({
-        name: '',
-        ratings: '',
-        duration: ''
-      });
-    }
-  }
-  
+   
 
   return (
     <div>
       <h8k-navbar header={ title } />
       <div className='layout-row justify-content-center mt-100'>
         <div className='w-30 mr-75'>
-          <Movieform changeHandler={changeHandler} formInput={formInput} handleSubmit={handleSubmit}/>
+          <Movieform addMovie={addMovie} />
         </div>
         <div className='layout-column w-30'>
           <Search />
--- src/components/Movieform.js
+++ src/components/Movieform.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-function Movieform({changeHandler, formInput, handleSubmit}) {
+function Movieform({addMovie}) {
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState("");
+  const [duration, setDuration] = useState("");
+  const [error, setError] = useState(false);
+
+  const checkFormInput = () => {
+    if (name === "" || rating === "" || duration === ""){
+      return;
+    }
+    const tDuration = duration.slice(-1);
+    if(tDuration === "m" || tDuration === "h"){
+      const tValue = Number(duration.slice(0, duration.length -1));
+      setError(false);
+      if(tDuration === "m"){
+        const tInMinutes = tValue /60;
+        const tInHours = (Math.round(tInMinutes * 10) /10).toFixed(1);
+        addMovie(name, Number(rating), tInHours);
+      } else {
+        addMovie(name, Number(rating), tValue);
+      }
+    } else {
+      setName("");
+      setRating("");
+      setDuration("");
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    if(name !== "" || rating !== "" || duration !== "") setError(false);
+  }, [name, rating, duration]);
 
   return (
     <section>
       <div className='card pa-30'>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => e.preventDefault()}>
           <div className='layout-column mb-15'>
             <label htmlFor='name' className='mb-3'>Movie Name</label>
             <input 
@@ -13,8 +44,8 @@ function Movieform({changeHandler, formInput, handleSubmit}) {
               id='name'
               placeholder='Enter Movie Name'
               data-testid='nameInput'
-              value={formInput.name}
-              onChange={changeHandler}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className='layout-column mb-15'>
@@ -26,8 +57,8 @@ function Movieform({changeHandler, formInput, handleSubmit}) {
               max="100"
               placeholder='Enter Rating on a scale of 1 to 100'
               data-testid='ratingsInput'
-              value={formInput.ratings}
-              onChange={changeHandler}
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
             />
           </div>
           <div className='layout-column mb-30'>
@@ -37,8 +68,8 @@ function Movieform({changeHandler, formInput, handleSubmit}) {
               id='duration'
               placeholder='Enter duration in hours or minutes'
               data-testid='durationInput'
-              value={formInput.duration}
-              onChange={changeHandler}
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
             />
           </div>
           {/* Use this div when time format is invalid */}
@@ -53,6 +84,7 @@ function Movieform({changeHandler, formInput, handleSubmit}) {
               type='submit'
               className='mx-0'
               data-testid='addButton'
+              onClick={checkFormInput}
             >
               Add Movie
             </button>
